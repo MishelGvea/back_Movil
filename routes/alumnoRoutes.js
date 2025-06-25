58572d4e936cd3e5a7e23286aa042e176787e6d9
Markos TYS <152444607+MarcosUriel123@users.noreply.github.com>
2025-06-25 08:43:49 -0600

    SOLICITO AYUDA EN BACK

--- routes/alumnoRoutes.js
+++ routes/alumnoRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerDatosAlumno, cambiarContrasena, editarPerfil } = require('../controllers/alumnoController');
+const { obtenerDatosAlumno, cambiarContrasena, editarPerfil, obtenerActividadesPorAlumno } = require('../controllers/alumnoController');
 
 // Obtener datos del alumno
 router.get('/:matricula', obtenerDatosAlumno);
 
 // Cambiar contraseña
 router.put('/cambiar-contrasena/:matricula', cambiarContrasena);
+
+// Obtener actividades por alumno
+// Cambiar la ruta para incluir el parámetro de materia
+router.get('/actividades/:matricula/:materia', obtenerActividadesPorAlumno);
 
 module.exports = router;
